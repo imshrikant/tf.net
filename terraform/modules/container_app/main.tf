776e473d32a18e0206b441cc714b500ec658fbf4
ShrikantInfoSwifts <shrikant.b@infoswift.com>
2025-11-17 09:59:29 +0530

    Updated the cont app module .

--- terraform/modules/container_app/main.tf
+++ terraform/modules/container_app/main.tf
@@ -1,33 +1,26 @@
-resource "azurerm_container_app_environment" "env" {
-  name                = "${var.app_name}-env"
-  location            = var.location
-  resource_group_name = var.rg_name
-  tags                = var.tags
-}
-
 resource "azurerm_container_app" "app" {
-  name                         = var.app_name
-  resource_group_name          = var.rg_name
+  name                = var.app_name
+  resource_group_name = var.rg_name
   container_app_environment_id = azurerm_container_app_environment.env.id
-  revision_mode                = "Single"
-
-  template {
-    container {
-      name   = "api"
-      image  = var.container_image // GitHub will push this or reference public
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
-  }
+  location            = var.location
 
   ingress {
     external_enabled = true
-    target_port      = 8080
+    target_port      = 80
+
+    traffic_weight {
+      weight = 100
+    }
+  }
+
+  template {
+    container {
+      name   = var.app_name
+      image  = var.container_image
+      cpu    = 0.5
+      memory = "1Gi"
+    }
   }
 
   tags = var.tags
-}
-
-output "app_url" {
-  value = azurerm_container_app.app.ingress[0].fqdn
 }
---
+++ terraform/modules/container_app/outputs.tf
@@ -0,0 +1,3 @@
+output "url" {
+  value = azurerm_container_app.app.latest_revision_fqdn
+}
