4a63bb50a452d0663129ec755830037854649e42
ShrikantInfoSwifts <shrikant.b@infoswift.com>
2025-11-17 10:36:07 +0530

    Updated main.tf in cont app.

--- terraform/modules/container_app/main.tf
+++ terraform/modules/container_app/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_container_app_environment" "env" {
 
 resource "azurerm_container_app" "app" {
   name                         = var.app_name
-  container_app_environment_id = azurerm_container_app_environment.env.id
+  container_app_environment_id = var.env_id
   resource_group_name          = var.rg_name
 
   revision_mode = "Single"
@@ -17,7 +17,7 @@ resource "azurerm_container_app" "app" {
     external_enabled = true
     target_port      = 80
 
-    traffic {
+    traffic_weight {
       latest_revision = true
       percentage      = 100
     }
